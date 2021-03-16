287b964aea120853ece5c7db12014636d42a13b2
Mohammad Reza Vafazadeh <mrvafazadeh78@icloud.com>
2021-03-16 21:37:52 +0330

    Added some comments

--- database.py
+++ database.py
@@ -1,6 +1,18 @@
+"""
+This file is responsible for creating, inserting, and editing from the database.
+This database has two tables.
+    1. URL (id INTEGER PRIMARY KEY,
+           URL text not null unique,
+           date DateTime DEFAULT CURRENT_TIMESTAMP)
+    2. counter (id INTEGER)
+
+The URLs has unique id and DateTime. DateTime is the time that a URL will add to the database.
+The counter is the index that will get the URL from the database and extract links from that site.
+"""
 import sqlite3
 
 
+# Create database and table for saving url
 def create_table():
     con = sqlite3.connect('urls.db')
     cur = con.cursor()
@@ -17,6 +29,61 @@ def create_table():
     con.close()
 
 
+# Insert url to database
+def insert_url(url):
+    con = sqlite3.connect('urls.db')
+    cur = con.cursor()
+
+    cur.execute("insert into urls (url) values (?)", (url,))
+
+    con.commit()
+
+    con.close()
+
+
+# Returns True if url exist in database, else False.
+def url_exists(url):
+    con = sqlite3.connect('urls.db')
+    cur = con.cursor()
+
+    cur.execute("select url from urls where url = ?", (url,))
+
+    res = cur.fetchone()
+
+    con.close()
+
+    return res is not None
+
+
+# Get url by id from database
+def get_url_by_id(_id):
+    con = sqlite3.connect('urls.db')
+    cur = con.cursor()
+
+    cur.execute("select url from urls where id = ?", (_id,))
+
+    res = cur.fetchone()
+
+    con.close()
+
+    return res[0].strip()
+
+
+# Get urls length. (last id number for url)
+def get_urls_length():
+    con = sqlite3.connect('urls.db')
+    cur = con.cursor()
+
+    cur.execute("select count(url) from urls", )
+
+    res = cur.fetchone()
+
+    con.close()
+
+    return res[0]
+
+
+# Set counter to zero and store to database
 def set_counter():
     con = sqlite3.connect('urls.db')
     cur = con.cursor()
@@ -32,6 +99,7 @@ def set_counter():
     con.close()
 
 
+# Increment counter (counter += 1) and store to database
 def increment_counter():
     con = sqlite3.connect('urls.db')
     cur = con.cursor()
@@ -42,6 +110,7 @@ def increment_counter():
     con.close()
 
 
+# Get number of counter
 def get_counter():
     con = sqlite3.connect('urls.db')
     cur = con.cursor()
@@ -52,53 +121,3 @@ def get_counter():
         cur.execute("select id from counter")
         _id = cur.fetchone()
     return _id[0]
-
-
-def insert_url(url):
-    con = sqlite3.connect('urls.db')
-    cur = con.cursor()
-
-    cur.execute("insert into urls (url) values (?)", (url,))
-
-    con.commit()
-
-    con.close()
-
-
-def url_exists(url):
-    con = sqlite3.connect('urls.db')
-    cur = con.cursor()
-
-    cur.execute("select url from urls where url = ?", (url,))
-
-    res = cur.fetchone()
-
-    con.close()
-
-    return res is not None
-
-
-def get_url_by_id(_id):
-    con = sqlite3.connect('urls.db')
-    cur = con.cursor()
-
-    cur.execute("select url from urls where id = ?", (_id,))
-
-    res = cur.fetchone()
-
-    con.close()
-
-    return res[0].strip()
-
-
-def get_urls_length():
-    con = sqlite3.connect('urls.db')
-    cur = con.cursor()
-
-    cur.execute("select count(url) from urls", )
-
-    res = cur.fetchone()
-
-    con.close()
-
-    return res[0]
